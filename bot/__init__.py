a59b6bb0dcb40155b027defe039413da3015b306
evxr123 <kalidevoo10@gmail.com>
2025-01-17 11:03:53 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "27861872"))
+    API_HASH = os.environ.get("API_HASH", "29a5d982bfd2b0a06dddb1f85361cfa9")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7505512532:AAFp3nTU6iw9DOGITcaWrgsHAUgTfpR15Mg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Kali_Devbot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
